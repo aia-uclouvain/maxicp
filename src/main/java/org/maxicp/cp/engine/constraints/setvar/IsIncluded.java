640bac1f4240fda1138bd36e12cd103bd95e6b8f
Amaury Guichard <reven.myst@gmail.com>
2025-04-22 16:03:03 +0200

    Tests on set constraints

--- src/main/java/org/maxicp/cp/engine/constraints/setvar/IsIncluded.java
+++ src/main/java/org/maxicp/cp/engine/constraints/setvar/IsIncluded.java
@@ -8,11 +8,12 @@ package org.maxicp.cp.engine.constraints.setvar;
 
 import org.maxicp.cp.engine.core.AbstractCPConstraint;
 import org.maxicp.cp.engine.core.CPBoolVar;
+import org.maxicp.cp.engine.core.CPSetVar;
 import org.maxicp.cp.engine.core.CPSetVarImpl;
 
 public class IsIncluded extends AbstractCPConstraint {
 
-    CPSetVarImpl set;
+    CPSetVar set;
     int v;
     CPBoolVar bool;
 
@@ -22,7 +23,7 @@ public class IsIncluded extends AbstractCPConstraint {
      * @param v    the value to be included/excluded
      * @param set  the set variable
      */
-    public IsIncluded(CPBoolVar bool, int v, CPSetVarImpl set) {
+    public IsIncluded(CPBoolVar bool, int v, CPSetVar set) {
         super(set.getSolver());
         this.set = set;
         this.v = v;
