ebd37cfa5d0f654fcfd63df1983d6d3f14d43aa5
pschaus <pschaus@gmail.com>
2025-07-17 17:02:33 +0200

    start clean costcard

--- src/main/java/org/maxicp/cp/engine/constraints/CostCardinalityMaxDC.java
+++ src/main/java/org/maxicp/cp/engine/constraints/CostCardinalityMaxDC.java
@@ -12,10 +12,18 @@ import org.maxicp.cp.engine.core.CPIntVar;
 import org.maxicp.state.StateInt;
 import org.maxicp.util.exception.InconsistencyException;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
+/**
+ * Implementation of the Global Cardinality Constraint with Costs from the paper
+ *
+ * Margaux Schmied, Jean-Charles Régin:
+ * Efficient Implementation of the Global Cardinality Constraint with Costs. CP 2024
+ *
+ * @author  Margaux Schmied
+ */
 public class CostCardinalityMaxDC extends AbstractCPConstraint {
 
     private final CPIntVar[] x;
@@ -41,24 +49,26 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
     private int edgeCount;
     private int costArcMax;
 
-    private int[][] domains;
-    private int[] numValuesByDomain;
+    private int[][] domain;
+    private int[] domainSize;
 
-    private int[][] dist;
+    private long[][] dist; // dist[i][j] is the shortest distance from node i to node j in the residual graph
+    private long[][] distReverse; // distReverse[i][j] is the shortest distance from node j to node i in the reverse residual graph
 
     private SCC scc;
     private List<List<Integer>> sccs;
     int[] sccByNode;
     int[] pivots;
-    int[] distMaxIn;
-    int[] distMaxOut;
+    long[] distMaxIn;
+    long[] distMaxOut;
 
     /**
      * Constraint the maximum number of occurrences of a range of values in x.
      *
-     * @param x     The variables to constraint (at least one)
+     * @param x     The variables to constraint (at least one), only non-negative values
      * @param upper The upper cardinality bounds,
      *              upper[i] is the maximum number of occurrences of value i in x
+     *              The size of upper must be equal to the number of columns in costs and equal to the larest value in x.
      * @param costs The costs associated with each value in x.
      */
     public CostCardinalityMaxDC(CPIntVar[] x, int upper[], int[][] costs, CPIntVar H) {
@@ -73,6 +83,11 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
             if (upper[i] < 0) throw new IllegalArgumentException("upper bounds must be non negative" + upper[i]);
             this.upper[i] = upper[i];
         }
+        // largest value in x
+        int largest = Stream.of(x).mapToInt(var -> var.max()).max().getAsInt();
+        if (nValues < largest) {
+            throw new IllegalArgumentException("upper bounds length must be at least as large as the largest value in x");
+        }
         if (costs.length != x.length) {
             throw new IllegalArgumentException("costs must have the same length as upper bounds");
         }
@@ -83,6 +98,9 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
         for (int i = 0; i < nVars; i++) {
             assignment[i] = getSolver().getStateManager().makeStateInt(-1); // -1 means unassigned
             this.x[i] = x[i];
+            if (x[i].min() < 0) {
+                throw new IllegalArgumentException("variable domains must be non-negative");
+            }
         }
 
         numNodes = nValues + nVars + 2; // source, variables, values, sink
@@ -96,10 +114,12 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
         edges = new int[numNodes * numNodes][3];
         edgesReverse = new int[numNodes * numNodes][3];
 
-        domains = new int[nVars][nValues];
-        numValuesByDomain = new int[nVars];
+        // domain[i][0..domainSize[i]-1] contains the values of variable i
+        domain = new int[nVars][nValues];
+        domainSize = new int[nVars];
 
-        dist = new int[numNodes][numNodes];
+        dist = new long[numNodes][numNodes];
+        distReverse = new long[numNodes][numNodes];
 
         scc = new SCC(numNodes);
     }
@@ -110,7 +130,6 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
             if (!var.isFixed())
                 var.propagateOnDomainChange(this);
         }
-
         propagate();
     }
 
@@ -119,6 +138,8 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
     public void propagate() {
 
         cleanDataStructures();
+
+        updateDomains();
 
         buildNetworkFlow(costNetworkFlow, capMaxNetworkFlow);
 
@@ -131,10 +152,9 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
 
         // fill the assignment
         for (int i = 0; i < nVars; i++) { // from variables to values
-            numValuesByDomain[i] = x[i].fillArray(domains[i]);
-            for (int j = 0; j < numValuesByDomain[i]; j++) {
-                if (minCostMaxFlow.getFlow()[i + 1][nVars + 1 + domains[i][j]] > 0) {
-                    assignment[i].setValue(domains[i][j]); // assign the value to the variable
+            for (int j = 0; j < domainSize[i]; j++) {
+                if (minCostMaxFlow.getFlow()[i + 1][nVars + 1 + domain[i][j]] > 0) {
+                    assignment[i].setValue(domain[i][j]); // assign the value to the variable
                     break; // only one value can be assigned to a variable
                 }
             }
@@ -146,14 +166,19 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
 
     }
 
+    private void updateDomains() {
+        for (int i = 0; i < nVars; i++) { // from variables to values
+            domainSize[i] = x[i].fillArray(domain[i]);
+        }
+    }
+
     private void cleanDataStructures() {
         edgeCount = 0;
-        costArcMax=0;
+        costArcMax = 0;
         for (int i = 0; i < numNodes; i++) {
             Arrays.fill(capMaxResidualGraph[i], 0);
             Arrays.fill(costResidualGraph[i], 0);
             Arrays.fill(dist[i], Integer.MAX_VALUE);
-
         }
     }
 
@@ -164,19 +189,19 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
         scc.findSCC(capMaxResidualGraph);
         sccs = scc.getComposantes();
         sccByNode = scc.getSCCByNode();
-        distMaxIn = new int[sccs.size()];
-        distMaxOut = new int[sccs.size()];
+        distMaxIn = new long[sccs.size()];
+        distMaxOut = new long[sccs.size()];
 
         selectPivotBySCC(sccs);
 
         // For each SCC, compute the shortest path from the pivot to all other nodes and from all nodes to the pivot
         for (int indexSCC = 0; indexSCC < pivots.length; indexSCC++) {
             int pivot = pivots[indexSCC];
-            dist[pivot] = bellmanFord(edgeCount, edges, pivot); // Compute shortest path from pivot to all nodes
-            int[] distPivotRev = bellmanFord(edgeCount, edgesReverse, pivot); // Compute shortest path from all nodes to pivot
+            bellmanFord(edgeCount, edges, pivot, dist[pivot]); // Compute shortest path from pivot to all nodes
+            bellmanFord(edgeCount, edgesReverse, pivot, distReverse[pivot]); // Compute shortest path from all nodes to pivot
             for (int i = 0; i < numNodes; i++) {
-                if (distPivotRev[i] != 0) {
-                    dist[i][pivot] = distPivotRev[i]; // Store the reverse distance
+                if (distReverse[pivot][i] != 0) {
+                    dist[i][pivot] = distReverse[pivot][i]; // Store the reverse distance
                 }
                 if (distMaxOut[indexSCC] < dist[pivot][i] && dist[pivot][i] != Integer.MAX_VALUE) {
                     distMaxOut[indexSCC] = dist[pivot][i]; // Maximum distance from pivot to any node in the SCC
@@ -184,26 +209,24 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
                 if (distMaxIn[indexSCC] < dist[i][pivot] && dist[i][pivot] != Integer.MAX_VALUE) {
                     distMaxIn[indexSCC] = dist[i][pivot]; // Maximum distance from any node in the SCC to the pivot
                 }
-
             }
         }
 
 
         for (int i = 0; i < nVars; i++) {
             int varNode = i + 1; // node representing variable i
-
             //iterate over all values of variable i
-            for (int j = 0; j < numValuesByDomain[i]; j++) {
-                int valueNode = nVars + 1 + domains[i][j]; // node representing value j of variable i
+            for (int j = 0; j < domainSize[i]; j++) {
+                int valueNode = nVars + 1 + domain[i][j]; // node representing value j of variable i
                 int indexSCC = sccByNode[valueNode];
 
-                if (assignment[i].value() == domains[i][j])
+                if (assignment[i].value() == domain[i][j])
                     continue; // skip if already assigned
 
                 if (sccByNode[varNode] != sccByNode[valueNode] || indexSCC == -1) {
                     // there is no way to reach varNode from valueNode so:
                     // Arc is not consistent, remove it
-                    x[i].remove(domains[i][j]);
+                    x[i].remove(domain[i][j]);
                     continue;
                 }
                 // Check the SCC with the pivot
@@ -218,12 +241,12 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
                 }
 
                 if (dist[valueNode][valueNode] == Integer.MAX_VALUE) { // If the distance is not computed yet, compute it
-                    dist[valueNode] = bellmanFord(edgeCount, edges, valueNode);
+                    bellmanFord(edgeCount, edges, valueNode, dist[valueNode]);
                 }
                 // Check if the arc (varNode, valueNode) is consistent with Régin 2002
                 if (dist[valueNode][varNode] > H.max() - minCostAssignment - costResidualGraph[varNode][valueNode]) { //dR(f)(u,v)=dRs(f)(u,v)−dR(f)(s,u)+dR(f)(s,v)
                     // Arc is not consistent, remove it
-                    x[i].remove(domains[i][j]);
+                    x[i].remove(domain[i][j]);
                 }
             }
         }
@@ -235,13 +258,13 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
         for (int i = 0; i < numNodes; i++) {
             for (int j = 0; j < numNodes; j++) {
                 if (capMaxResidualGraph[i][j] > 0) {
-                    edges[edgeCount][0]=i;
-                    edges[edgeCount][1]=j;
-                    edges[edgeCount][2]=costResidualGraph[i][j];
+                    edges[edgeCount][0] = i;
+                    edges[edgeCount][1] = j;
+                    edges[edgeCount][2] = costResidualGraph[i][j];
 
-                    edgesReverse[edgeCount][0]=j;
-                    edgesReverse[edgeCount][1]=i;
-                    edgesReverse[edgeCount][2]=costResidualGraph[i][j];
+                    edgesReverse[edgeCount][0] = j;
+                    edgesReverse[edgeCount][1] = i;
+                    edgesReverse[edgeCount][2] = costResidualGraph[i][j];
 
                     edgeCount++;
                 }
@@ -284,8 +307,7 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
     }
 
     private void builResidualGraph(int[][] capMaxNetworkFlow, int[][] costNetworkFlow, int[][] flow) {
-
-
+        // Build the residual graph from the flow and the original network flow capacities and costs
         for (int i = 0; i < numNodes; i++) {
             for (int j = 0; j < numNodes; j++) {
                 if (capMaxNetworkFlow[i][j] > 0 && flow[i][j] < capMaxNetworkFlow[i][j]) {
@@ -309,10 +331,9 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
         for (int i = 0; i < nVars; i++) { // from variables to values
             Arrays.fill(capMaxNetworkFlow[i + 1], 0);
             Arrays.fill(costNetworkFlow[i + 1], 0);
-            numValuesByDomain[i] = x[i].fillArray(domains[i]);
-            for (int j = 0; j < numValuesByDomain[i]; j++) {
-                capMaxNetworkFlow[i + 1][nVars + 1 + domains[i][j]] = 1; // capacity of 1 for each variable to value edge
-                costNetworkFlow[i + 1][nVars + 1 + domains[i][j]] = costs[i][domains[i][j]]; // cost of assigning variable i to value j
+            for (int j = 0; j < domainSize[i]; j++) {
+                capMaxNetworkFlow[i + 1][nVars + 1 + domain[i][j]] = 1; // capacity of 1 for each variable to value edge
+                costNetworkFlow[i + 1][nVars + 1 + domain[i][j]] = costs[i][domain[i][j]]; // cost of assigning variable i to value j
             }
         }
 
@@ -321,15 +342,15 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
         }
     }
 
-    private int[] bellmanFord(int edgeCount, int[][] edges, int src) {
-
-
+    /**
+     * Shortest path distance from the src to all other nodes using Bellman-Ford algorithm.
+     * Return null if a negative cycle is detected.
+     */
+    private void bellmanFord(int edgeCount, int[][] edges, int src, long[] dist) {
         // Initially distance from source to all other vertices
         // is not known(Infinite).
-        int[] dist = new int[numNodes];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[src] = 0;
-
         // Relaxation of all the edges V times, not (V - 1) as we
         // need one additional relaxation to detect negative cycle
         for (int i = 0; i < numNodes; i++) {
@@ -338,16 +359,15 @@ public class CostCardinalityMaxDC extends AbstractCPConstraint {
                 int v = edges[ne][1];
                 int wt = edges[ne][2];
                 if (dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]) {
-                    // If this is the Vth relaxation, then there is
-                    // a negative cycle
-                    if (i == numNodes - 1)
-                        return null;
+                    // V_th relaxation => negative cycle
+                    if (i == numNodes - 1) {
+                        throw InconsistencyException.INCONSISTENCY;
+                    }
                     // Update shortest distance to node v
                     dist[v] = dist[u] + wt;
                 }
             }
         }
-        return dist;
     }
 
     public int getMinCostAssignment() {
