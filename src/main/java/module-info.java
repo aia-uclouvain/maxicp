a0f4e2b24e643c0874dec584c451b576507194c0
pschaus <pschaus@gmail.com>
2025-09-03 17:02:26 +0200

    clean modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,22 @@
 module org.maxicp {
     exports org.maxicp;
     exports org.maxicp.cp;
+    exports org.maxicp.cp.modeling;
+    exports org.maxicp.cp.engine.constraints;
+    exports org.maxicp.cp.engine.constraints.scheduling;
+    exports org.maxicp.cp.engine.constraints.seqvar;
+    exports org.maxicp.cp.engine.constraints.setvar;
+    exports org.maxicp.cp.engine.core;
     exports org.maxicp.modeling;
     exports org.maxicp.search;
     exports org.maxicp.state;
     exports org.maxicp.util;
+
+               // Add this line
+    exports org.maxicp.modeling.algebra.bool;           // Add this line
+    exports org.maxicp.util.exception;
+
+
     requires org.json;
     requires xcsp3.tools;
     requires java.xml;
--- src/main/java/org/maxicp/cp/engine/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * MaxiCP is under MIT License
- * Copyright (c)  2023 UCLouvain
- */
-
-/**
- * Solver and Constraint objects
- */
-package org.maxicp.cp.engine;
