5dbadd28cba2057468d27c2f14a020d6cfbd505b
pschaus <pschaus@gmail.com>
2025-09-30 08:28:29 +0200

    improve test min card

--- src/test/java/org/maxicp/cp/engine/constraints/CardinalityMinFWCTest.java
+++ src/test/java/org/maxicp/cp/engine/constraints/CardinalityMinFWCTest.java
@@ -16,6 +16,7 @@ import org.maxicp.search.DFSearch;
 import org.maxicp.search.SearchStatistics;
 import org.maxicp.search.Searches;
 
+import java.util.Random;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -63,12 +64,11 @@ public class CardinalityMinFWCTest extends CPSolverTest {
 
     @ParameterizedTest
     @MethodSource("getSolver")
-    public void testRandomDecomp(CPSolver cp) {
+    public void testDecomp1(CPSolver cp) {
 
         Set<Integer>[] initialDomains = new Set[]{Set.of(0, 1, 2, 3), Set.of(0, 1, 2, 3), Set.of(0, 1, 2, 3, 4, 5), Set.of(2, 5), Set.of(0, 1, 2, 3)};
         CPIntVar[] x = CPFactory.makeIntVarArray(initialDomains.length, i -> CPFactory.makeIntVar(cp, initialDomains[i]));
 
-        CPIntVar[] count = CPFactory.makeIntVarArray(cp, 4, 5);
         int[] minCard = new int[]{0, 1, 2, 0};
 
         // -----------------------------
@@ -86,14 +86,88 @@ public class CardinalityMinFWCTest extends CPSolverTest {
 
         for (int i = 0; i < 4; i++) {
             int v = i;
-            cp.post(CPFactory.eq(count[i], CPFactory.sum(CPFactory.makeIntVarArray(x.length, j -> CPFactory.isEq(x[j], v)))));
-            cp.post(CPFactory.ge(count[i], minCard[v]));
+            cp.post(CPFactory.ge(CPFactory.sum(CPFactory.makeIntVarArray(x.length, j -> CPFactory.isEq(x[j], v))), minCard[v]));
         }
 
         search = CPFactory.makeDfs(cp, Searches.staticOrder(x));
         SearchStatistics stats2 = search.solve();
 
         assertEquals(stats1.numberOfSolutions(), stats2.numberOfSolutions());
+
+    }
+
+    /**
+     * Generates a random set of integers from 0 to maxVal with given density
+     */
+    private Set<Integer> randomSet(int maxVal, double density, Random rand) {
+        Set<Integer> s = new java.util.HashSet<>();
+        for (int v = 0; v <= maxVal; v++) {
+            if (rand.nextDouble() < density) {
+                s.add(v);
+            }
+        }
+        if (s.isEmpty()) s.add(rand.nextInt(maxVal + 1));
+        return s;
+    }
+
+
+
+
+    @ParameterizedTest
+    @MethodSource("getSolver")
+    public void testRandomDecomp2(CPSolver cp) {
+
+
+        int n = 6;
+        int d = 4;
+        double density = 0.5;
+
+        for (int iter = 0; iter < 20; iter++) {
+
+            java.util.Random rand = new java.util.Random(iter);
+
+            Set<Integer>[] initialDomains = new Set[n];
+            for (int i = 0; i < n; i++) {
+                initialDomains[i] = randomSet(d, density, rand);
+            }
+            CPIntVar[] x = CPFactory.makeIntVarArray(n, i -> CPFactory.makeIntVar(cp, initialDomains[i]));
+            int[] minCard  = new int[d];
+            // random cardinality
+
+            for (int v = 0; v < d; v++) {
+                minCard[v] = rand.nextInt(n / d);
+            }
+
+            // ------------------- with global constraint ----------------
+
+            cp.getStateManager().saveState();
+
+            cp.post(new CardinalityMinFWC(x, minCard));
+
+            DFSearch search = CPFactory.makeDfs(cp, Searches.staticOrder(x));
+            SearchStatistics stats1 = search.solve();
+
+            // ------------------- decomposition ----------------
+
+            cp.getStateManager().restoreState();
+
+            for (int i = 0; i < 4; i++) {
+                int v = i;
+                cp.post(CPFactory.ge(CPFactory.sum(CPFactory.makeIntVarArray(x.length, j -> CPFactory.isEq(x[j], v))), minCard[v]));
+            }
+
+            search = CPFactory.makeDfs(cp, Searches.staticOrder(x));
+            SearchStatistics stats2 = search.solve();
+
+            // compare number of solutions
+
+            assertEquals(stats1.numberOfSolutions(), stats2.numberOfSolutions());
+
+        }
+
+
+
+
 
     }
 }
