9e2bb684a08f196d3d8cd9cef15955f98539bb05
pschaus <pschaus@gmail.com>
2026-02-26 14:17:02 +0000

    TSP LNS, RelaxedSequence constraint

--- src/main/java/org/maxicp/cp/examples/raw/tsp/TSPSeqVarLNS.java
+++ src/main/java/org/maxicp/cp/examples/raw/tsp/TSPSeqVarLNS.java
@@ -8,6 +8,7 @@ package org.maxicp.cp.examples.raw.tsp;
 
 import org.maxicp.cp.CPFactory;
 import org.maxicp.cp.engine.constraints.seqvar.Distance;
+import org.maxicp.cp.engine.constraints.seqvar.RelaxedSequence;
 import org.maxicp.cp.engine.core.CPIntVar;
 import org.maxicp.cp.engine.core.CPSeqVar;
 import org.maxicp.cp.engine.core.CPSolver;
@@ -18,8 +19,12 @@ import org.maxicp.search.Objective;
 import org.maxicp.search.SearchStatistics;
 import org.maxicp.util.algo.DistanceMatrix;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.maxicp.cp.CPFactory.*;
 import static org.maxicp.search.Searches.*;
@@ -29,6 +34,7 @@ import static org.maxicp.search.Searches.*;
  * <a href="https://en.wikipedia.org/wiki/Travelling_salesman_problem">Wikipedia</a>.
  */
 public class TSPSeqVarLNS {
+
 
 
     public static void main(String[] args) {
@@ -52,17 +58,17 @@ public class TSPSeqVarLNS {
         for (int node = 0; node < n + 1; node++) {
             tour.require(node);
         }
+
         // capture the distance traveled according to the distance matrix
         cp.post(new Distance(tour, distance, totLength));
+
         // objective consists in minimizing the traveled distance
         Objective obj = cp.minimize(totLength);
 
         // ================ best sol (LNS) ==================
 
-        int [] bestSol = new int[n+1];
-        for (int i = 0; i <= n; i++) {
-            bestSol[i] = i;
-        }
+        // best so far tour
+        int [] bestTour = IntStream.range(0,n+1).toArray();
 
         // ===================== search =====================
 
@@ -74,7 +80,7 @@ public class TSPSeqVarLNS {
         long init = System.currentTimeMillis();
         dfs.onSolution(() -> {
             // update best solution
-            tour.fillNode(bestSol, SeqStatus.MEMBER_ORDERED);
+            tour.fillNode(bestTour, SeqStatus.MEMBER_ORDERED);
             System.out.println(totLength);
         });
 
@@ -84,20 +90,24 @@ public class TSPSeqVarLNS {
         for (int iter = 0; iter < 100; iter++) {
             System.out.println(String.format("---restart %d ----", iter));
             dfs.optimizeSubjectTo(obj, s -> false, () -> {
+                Set<Integer> relaxed = randomSubset(random, 0, n, 5);
                 // relax current solution
-                int current = 0;
-                for (int i = 1; i < n - 1; i++) {
-                    int nextNode = bestSol[i];
-                    if (random.nextInt(100) < percentageOfFixed) {
-                        if (!tour.isNode(nextNode, SeqStatus.MEMBER)) {
-                            // the node may have been inserted already, if not, insert it
-                            cp.post(CPFactory.insert(tour, current, nextNode));
-                        }
-                        current = nextNode;
-                    }
-                }
+                cp.post(new RelaxedSequence(tour, bestTour, relaxed));
             });
         }
+    }
+
+    /**
+     * Random selection of a set of cardinality size on the range [from..to-1]
+     */
+    public static Set<Integer> randomSubset(Random rand, int from, int to, int size) {
+        if (size > to-from)
+            throw new IllegalArgumentException(String.format("size %d > %d", size, to-from));
+        Set<Integer> res = new HashSet<>();
+        while (res.size() < size) {
+            res.add(rand.nextInt(to-from) + from);
+        }
+        return res;
     }
 
 }
